b7ca68434da4b70737ed6bef4d0188604122530c
AbbasLewa <abbasomerhassn@gmail.com>
2025-02-23 17:59:58 +0200

    table

---
+++ config.js
@@ -0,0 +1,4 @@
+
+
+export const PORT = 5555;
+export const mongoDBURL = "mongodb+srv://abbasomerhassn:ZInYG0FOjQjPjr78@book-stroe-mern.c7zmj.mongodb.net/?retryWrites=true&w=majority&appName=book-stroe-MERN";
